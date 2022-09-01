b331311711d1456ac0c9333e2b891a5153f64051
chinthaka <chinthakarukshan@gmail.com>
2022-09-01 23:44:46 +0530

    Fixed bug in the button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = props => {
-return (<button className={`${classes.button} ${props.className}`} type={props.type || 'submit'} onclick={props.onClick}>{props.children}</button>);
+return (<button className={`${classes.button} ${props.className}`} type={props.type || 'submit'} onClick={props.onClick}>{props.children}</button>);
 }
 
 export default Button;
