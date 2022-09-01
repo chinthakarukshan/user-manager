820b36e5646d222bc7a9454ce86aa9da64c52082
chinthaka <chinthakarukshan@gmail.com>
2022-09-01 23:42:39 +0530

    Using state to hold the entered values in the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,18 +6,29 @@ import classes from './AddUser.module.css';
 
 const AddUser = props => {
 
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(username, age);
+    };
 
+    const usernameChangeHandler = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setAge(event.target.value);
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input type='text' id='username'/>
+                <input type='text' id='username' onChange={usernameChangeHandler}/>
                 <label htmlFor='age'>Age</label>
-                <input type='number' id='age'/>
+                <input type='number' id='age' onChange={ageChangeHandler}/>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
